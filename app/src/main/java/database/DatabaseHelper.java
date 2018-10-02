70a5b4df5a3f1db2244d856c7db84beda619b5dd
Rguni <43431755+Rguni@users.noreply.github.com>
2018-10-02 16:51:23 +0300

    Initial commit

---
+++ app/src/main/java/database/DatabaseHelper.java
@@ -0,0 +1,119 @@
+package database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import database.model.Note;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    // DB Ver
+    private static final int DATABASE_VERSION = 1;
+
+    // DB Name
+    private static final String DATABASE_NAME = "notes_db";
+
+    public DatabaseHelper( Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // create notes table
+        db.execSQL(Note.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + Note.TABLE_NAME);
+
+        // Create tables again
+        onCreate(db);
+
+    }
+
+    public long insertNote(String note){
+
+        //get writable DB as we want to write data
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        // `id` and `timestamp` will be inserted automatically.
+        // no need to add them
+
+        values.put(Note.COLUMN_NOTE, note);
+
+        // insert row
+        long id = db.insert(Note.TABLE_NAME, null, values);
+
+        // close db connection
+        db.close();
+
+        return id;
+
+    }
+
+    public Note getNote(long id){
+        // get readable database as we are not inserting anything
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Note.TABLE_NAME,
+                new String[]{Note.COLUMN_ID, Note.COLUMN_NOTE, Note.COLUMN_TIMESTAMP},
+                Note.COLUMN_ID + "?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+
+        if (cursor!= null)
+            cursor.moveToFirst();
+
+        // prepare note object
+        Note note = new Note(
+                cursor.getInt(cursor.getColumnIndex(Note.COLUMN_ID)),
+                cursor.getString(cursor.getColumnIndex(Note.COLUMN_NOTE)),
+                cursor.getString(cursor.getColumnIndex(Note.COLUMN_TIMESTAMP)));
+
+        // close db connection
+        cursor.close();
+
+        // return notes list
+        return note;
+
+    }
+
+    public List<Note> getAllNotes(){
+
+        List<Note> notes = new ArrayList<>();
+
+        // Select All Query
+        String selectQuery = "SELECT * FROM " + Note.TABLE_NAME + " ORDER BY " +
+                Note.COLUMN_TIMESTAMP + " DESC";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()){
+            do{
+                Note note = new Note();
+                note.setId(cursor.getInt(cursor.getColumnIndex(Note.COLUMN_ID)));
+                note.setNote(cursor.getString(cursor.getColumnIndex(Note.COLUMN_NOTE)));
+                note.setTimestamp(cursor.getString(cursor.getColumnIndex(Note.COLUMN_TIMESTAMP)));
+
+                notes.add(note);
+            }while (cursor.moveToNext());
+        }
+
+        db.close();
+
+        return notes;
+    }
+
+}
